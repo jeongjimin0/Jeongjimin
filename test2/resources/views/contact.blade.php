f4422d633fe915b5e7f51a00d7843fcd8e1a6efc
jeongjimin0 <jjm8029@naver.com>
2023-01-16 17:12:10 +0900

    4

--- test2/resources/views/contact.blade.php
+++ test2/resources/views/contact.blade.php
@@ -14,23 +14,24 @@
       <li><a href="/" style="color: blue;">자기소개서</a></li>
       <li>｜<a href="/hello">&nbsp;&nbsp;포트폴리오</a></li>
       <li>｜<a href="/welcome">&nbsp;&nbsp;상세사항</a></li>
-    </nav>
+  </nav>
 
-    <div class="row">
-    <h5 class="project--one"> Projects </h5>
+  <div class="row">
+    <h5 class="project--one"><strong> Projects </strong> </h5>
+
     <dl class="row">
     <dt class="col-sm-3"><a href="/text">유지보수 시스템</dt></a>
-  <dd class="col-sm-9">유지보수 시스템 html, css 작성, 데이터베이스[Adminer] 연결</dd>
-  <dt class="col-sm-3">졸업작품</dt>
-  <dd class="col-sm-9">
+    <dd class="col-sm-9"> html, css 작성, 데이터베이스[Adminer] 연결</dd>
+    <dt class="col-sm-3">졸업작품</dt>
+    <dd class="col-sm-9">
     <p> for the term.</p>
     <p>And some more placeholder definition text.</p>
-  </dd>
-  <dt class="col-sm-3">Another term</dt>
-  <dd class="col-sm-9">This definition is short, so no extra paragraphs or anything.</dd>
+    </dd>
+    <dt class="col-sm-3">Another term</dt>
+    <dd class="col-sm-9">This definition is short, so no extra paragraphs or anything.</dd>
 
-  <dt class="col-sm-3">Nesting</dt>
-  <dd class="col-sm-9">
+    <dt class="col-sm-3">Nesting</dt>
+    <dd class="col-sm-9">
     <dl class="row">
       <dt class="col-sm-4">Nested definition list</dt>
       <dd class="col-sm-8">I heard you like definition lists. Let me put a definition list inside your definition list.</dd>
@@ -38,7 +39,16 @@
   </dd>
 </dl>
 
+<div class="row" style="padding-bottom: 60px;">
+  <h5 class="project--one"><strong> Github </strong></h5>
+  <dt class="col-sm-3">주소:</dt>
+  <dd class="col-sm-9"><a href="<?php echo url('https://github.com/jeongjimin0/Jeongjimin.git'); ?>">https://github.com/jeongjimin0/Jeongjimin.git</dd></a>
+</div>
+
+  <div class="row">
+    <h5 class="project--one"><strong> 자격증 </strong></h5>
   </div>
+
 </body>
 
 </div>
