917222c30772a271af4db0e18f29e5b0d4f7964a
Jeongjimin0 <jjm8029@naver.com>
2023-02-02 12:44:56 +0900

    react

---
+++ VScode/react-app/src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={'/read/' + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+function App() {
+  const mode = useState('WELCOME'); 
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' },
+  ];
+  let content = null;
+
+  if (mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }
+  else if (mode === 'READ'){
+    content = <Article title="READ" body="Hello, READ"></Article>
+  }
+
+  return (
+    <div>
+      <Header
+        title="WEB"
+        onChangeMode={() => {
+          mode = 'WELCOME';
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          mode = 'READ';
+        }}
+      ></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+    </div>
+  );
+}
+
+export default App;
